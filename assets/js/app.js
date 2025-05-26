e292fb5486fb9630a46c5dda6ca9a9ad240fea65
Oswaldoramirezb <oswaldo.ramirezbau@gmail.com>
2025-05-26 11:19:51 -0600

    Actualización de clase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,20 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $name = document.querySelector('name'); //La variables no coinciden con los elementos, la recomendación es que siempre se ocupen nombres de variables con nombres completos, no ocupar n, a, b, c, tienen que ser mas descriptivas
+const $blog = document.querySelector('#blog');
+const $location = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+async function displayUser(username) { //necesitamos agregar el async porque ocupamos await
+  try {
+    $name.textContent = 'cargando...'; //acomodo las variables de name, blog y location como aparecen arriba
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    console.log(data);
+    $name.textContent = '${data.name}';
+    $blog.textContent = '${data.blog}';
+    $location.textContent = '${data.location}';
+  } catch (error) {
+    handleError(error); //No estaba llamando antes en ningun momento si habia error
+  }
 }
 
 function handleError(err) {
